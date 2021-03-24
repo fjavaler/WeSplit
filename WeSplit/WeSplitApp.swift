19f314514a90f41244a60ca76c8a40865278425c
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-24 07:38:49 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Frederick Javalera on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
